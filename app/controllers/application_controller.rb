9c9c331f943cff7923a4e6b57836dabc9999893f
LoganBrannan <loganbrannan13@gmail.com>
2018-09-02 04:34:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+  	set :views, "app/views"
+  	set :public_dir, "public"
   end
-
+  
+    get "/" do
+  	erb :vitals
+  end
 end
